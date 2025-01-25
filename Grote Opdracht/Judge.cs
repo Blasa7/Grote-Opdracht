712f5d26c48340eb8c1f113aca6a5e5deabcdbfd
Mike27122003 <dummyraheem@gmail.com>
2025-01-25 15:32:52 +0100

    Made seperate Judge, Weight files

---
+++ Grote Opdracht/Judge.cs
@@ -0,0 +1,90 @@
+﻿public class Judge
+{
+    public int timeDelta;
+    public int timePenaltyDelta;
+    public int garbagePenaltyDelta;
+
+    public int totalTimePenalty;
+    public int totalGarbagePenalty;
+
+    public bool updatedPenalties = false;
+
+    public int minRoutes = 14;
+    public int maxRoutes = 16;
+
+    public double garbagePenaltyMultiplier = 4.32 * 7.3;
+    public double timePenaltyMultiplier = 1 * 0.07;
+
+    public float beginT;
+    public float endT;
+
+    Judgement judgement;
+
+    public float T;
+    public Random rng;
+
+    public Judge(Random rng)
+    {
+        this.rng = rng;
+
+        Reset();
+    }
+
+    public void Testify(int timeDelta, int timePenaltyDelta, int garbagePenaltyDelta)
+    {
+        this.timeDelta += timeDelta;
+        this.timePenaltyDelta += timePenaltyDelta;
+        this.garbagePenaltyDelta += garbagePenaltyDelta;
+    }
+
+    public void OverrideJudge(Judgement judgement)
+    {
+        this.judgement = judgement;
+    }
+
+    public Judgement GetJudgement()
+    {
+        if (judgement == Judgement.Undecided) //If no function has overidden the judgement
+        {
+            double maxWeightMultiplier = 9;
+            double weight = ((beginT - T) / beginT) * maxWeightMultiplier + 1;
+            double weightedGarbagePenalty = garbagePenaltyDelta * garbagePenaltyMultiplier * weight;
+            double weightedTimePenalty = timePenaltyDelta * timePenaltyMultiplier * weight;
+            double numerator = -(timeDelta + weightedTimePenalty + weightedGarbagePenalty);
+
+            double frac = numerator / T; // '-', because we want to minimize here
+            double res = Math.Exp(frac);
+            if (res >= rng.NextDouble())
+            {
+                judgement = Judgement.Pass;
+            }
+            else
+                judgement = Judgement.Fail;
+        }
+
+        if (!updatedPenalties && judgement == Judgement.Pass)
+        {
+            totalTimePenalty += timePenaltyDelta;
+            totalGarbagePenalty += garbagePenaltyDelta;
+            updatedPenalties = true;
+        }
+
+        return judgement;
+    }
+
+    public void Reset()
+    {
+        updatedPenalties = false;
+        timeDelta = 0;
+        timePenaltyDelta = 0;
+        garbagePenaltyDelta = 0;
+        judgement = Judgement.Undecided;
+    }
+}
+
+public enum Judgement
+{
+    Fail = 0,
+    Pass = 1,
+    Undecided = -1
+}
---
+++ Grote Opdracht/Weights.cs
@@ -0,0 +1,43 @@
+﻿public class Weights()
+{
+    public int addWeight = 100;
+    public int removeWeight = 50;
+    public int shuffleScheduleWeight = 200;
+    public int shuffleWorkDayWeight = 200;
+    public int shuffleRouteWeight = 200;
+
+    public int addWeightSum;
+    public int removeWeightSum;
+    public int shuffleScheduleSum;
+    public int shuffleWorkDayWeightSum;
+    public int shuffleRouteWeightSum;
+    public int totalWeightSum;
+
+    public void ResetWeights()
+    {
+        addWeight = 100;
+        removeWeight = 50;
+        shuffleScheduleWeight = 200;
+        shuffleWorkDayWeight = 200;
+        shuffleRouteWeight = 200;
+    }
+
+    public void RecalculateWeights()
+    {
+        addWeightSum = addWeight;
+        removeWeightSum = addWeightSum + removeWeight;
+        shuffleScheduleSum = removeWeightSum + shuffleScheduleWeight;
+        shuffleWorkDayWeightSum = shuffleScheduleSum + shuffleWorkDayWeight;
+        shuffleRouteWeightSum = shuffleWorkDayWeightSum + shuffleRouteWeight;
+        totalWeightSum = shuffleRouteWeightSum + 1;
+    }
+
+    public static Weights StartWeight()
+    {
+        Weights weights = new Weights();
+        weights.ResetWeights();
+        weights.RecalculateWeights();
+
+        return weights;
+    }
+}
